b0c6816f588436d9d9b338c22e5d4bc6f132a2e9
6thtouchrobotics <6thtouchrobotics@gmail.com>
2025-07-04 19:34:25 +0000

    updated the app.js file

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const { routers } = require('./routes/route');
 require('dotenv').config();
 const app = express();
 app.use(cors());
-app.use(routers());
 app.use(express.json());
-// Sync database and start server
+app.use(routers());
+
 sequelize.sync().then(() => {
     app.listen(3000, () => {
         console.log('Server running on http://localhost:3000');
